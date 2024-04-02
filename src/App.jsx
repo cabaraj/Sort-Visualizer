19643df4c50e15687c1a30f0efa069adb89cf570
Carlos Barajas <cabaraj2@uci.edu>
2024-04-01 17:15:57 -0700

    array of bars appear in screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import BubbleSortVisualizer from "./BubbleSort/BubbleSort"
 
 
 function App() {
 
   return (
     <>
-      
+      <h1>Visual Sorting</h1>
+      <BubbleSortVisualizer/>
+      what
     </>
   )
 }
