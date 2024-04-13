b870967eec93d6031f114a5350dd0c87a3c699e4
Carlos Barajas <cabaraj2@uci.edu>
2024-04-12 17:37:55 -0700

    use Bootstrap and set delay for swapping

--- src/sortVisualizer/SortVisualizer.jsx
+++ src/sortVisualizer/SortVisualizer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import "./BubbleSort.css";
-import DisplayBars from "./DisplayBars.js";
+import "./SortVisualizer.css";
+import DisplayBars from "./DisplayBars.jsx";
 
 const NUMBER_OF_BARS = 20;
 const INITIAL_COLOR = "blue";
 const COMPARING_COLOR = "red";
 const COMPLETED_COLOR = "green";
+const SWAP_SPEED_MS = 500;
 
-export default class BubbleSortVisualizer extends React.Component {
+export default class SortVisualizer extends React.Component {
     constructor(props) {
         super(props);
         
@@ -30,46 +31,67 @@ export default class BubbleSortVisualizer extends React.Component {
         for (let i = 0; i < NUMBER_OF_BARS; i++){
             array.push(getRandomInt())
         }
-        // init the array to have random integers
+        // init the array to have random integers to be represented with bars
         this.setState({array});
     }
 
+    //function to call sort algo and receives an array with the necessary swaps
+    startSort() {
+        const copyArray = [...this.state.array];
+        const swaps = [[0,1],[1,2]]; //call actual function using copyArray
+        
+        let i = 1;
+        for (let swap of swaps) {
+            setTimeout(swapBars, i*SWAP_SPEED_MS, swap);
+            i++;
+        }
+        
+    }
+    
+    //function that will return something to be displayed initially
     render() {
-        const {array} = this.state;
+        //const {array} = this.state;
+        const array = this.state.array;
         return (
-            <>
-                <DisplayBars array={array} barColor={INITIAL_COLOR}/>
-                <button onClick={() => this.initArray()}>Generate New Array</button>
-                <button onClick={() => colorBars()}>Color</button>
-                <button onClick={() => swapBars()}>Swap</button>
-            </>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Sorting Visualizer</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <DisplayBars array={array} barColor={INITIAL_COLOR}/>                        
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button onClick={() => this.initArray()}>Generate New Array</button>
+                        <button onClick={() => this.startSort()}>Sort</button>
+                    </div>                
+                </div>
+            </div>
         );
         
     }
 }
 
-/*
-function DisplayBars({ array, sorting=false }) {
-    let bgColor = (sorting === true) ? COMPARING_COLOR : INITIAL_COLOR;
+/* to show as if we change bars, we modify their height */
+function swapBars(pair) {
 
-    return (
-        <div className="array-container">
-            {array.map((value, idx) => (
-                <div className="array-bar" key={idx}
-                    style={{
-                        backgroundColor: bgColor,
-                        height: `${value*5}px`,
-                    }}>
-                </div>
-            ))}
-        </div>
-    );
+    const arrayBars = document.getElementsByClassName("array-bar");
+    const b1 = arrayBars[pair[0]].style;
+    const b2 = arrayBars[pair[1]].style;
+
+    [b1.height, b2.height] = [b2.height, b1.height];
 }
-*/
+
+
+
 
 /* to change the color of a bar:
-    get the style for the desired bar and change its background color*/
-function colorBars() {
+    get the style for the desired bar and change its background color
+    CHANGE BETWEEN BLACK AND RED
+    */
+function changeBarColor() {
     console.log('BARS')
     const arrayBars = document.getElementsByClassName("array-bar");
     const barOneStyle = arrayBars[1].style;
@@ -82,14 +104,3 @@ function colorBars() {
     //const bar = document
 }
 
-/* to change bars, we change their height */
-function swapBars() {
-
-    const arrayBars = document.getElementsByClassName("array-bar");
-    const b1 = arrayBars[0].style;
-    const b2 = arrayBars[1].style;
-    //console.log(arrayBars);
-    //arrayBars[0] = b2;
-    [b1.height, b2.height] = [b2.height, b1.height];
-    //console.log(arrayBars);
-}
